339b7cf85366fcc43f3908ff141975622cb10ac2
Masami Hiramatsu <masami.hiramatsu@gmail.com>
2012-03-20 01:10:58 +0900

    x86: Add bogus disassembler support
    Add a bogus x86 in-kernel disassember to dress up your
    panic message :)
    This time, the disassembler just supports basic
    instructions (no SSE, no AVX, no FPU etc.).
    However most of the case, it can show the code
    in kernel.
    
    Note that this currently shows assembler code in intel
    format, instead of gas-like att format.
    
    Signed-off-by: Masami Hiramatsu <masami.hiramatsu@gmail.com>

---
+++ arch/x86/include/asm/disasm.h
@@ -0,0 +1,20 @@
+#ifndef __X86_DISASM_H__
+#define __X86_DISASM_H__
+#include <asm/insn.h>
+
+/* Mnemonic format table lookup routines */
+extern const char *get_mnemonic_format(struct insn *insn, const char **grp);
+extern const char *get_prefix_name(struct insn *insn);
+
+/* Disassemble options */
+#define DISASM_PR_ADDR	1	/* Print address */
+#define DISASM_PR_RAW	2	/* Print raw code */
+#define DISASM_PR_ALL	(DISASM_PR_ADDR | DISASM_PR_RAW)
+
+/* Disassemble given decoded instruction */
+extern int disassemble(char *buf, size_t len, struct insn *insn);
+
+/* Put disassembled code with raw code and address into buffer */
+extern int snprint_assembly(char *buf, size_t len, struct insn *insn, int opts);
+
+#endif	/*__X86_DISASM_H__*/
