74c9662eaccbc983381f5c7806dcc8bdb56d6f35
Masami Hiramatsu <masami.hiramatsu@gmail.com>
2012-04-03 16:49:04 +0900

    x86/kdb: Add x86 disassembe command
    Add a simple 'dis' command for x86 on KDB.
    This command takes 2 arguments, the first one is the address
    and the second one is the length of disassembling bytes.
    
    Signed-off-by: Masami Hiramatsu <masami.hiramatsu@gmail.com>

--- arch/x86/kernel/dumpstack.c
+++ arch/x86/kernel/dumpstack.c
@@ -298,9 +298,9 @@ int __kprobes __die(const char *str, struct pt_regs *regs, long err)
 extern int disasm_on_oops;
 
 /* Find the instruction boundary address */
-static unsigned long find_instruction_boundary(unsigned long saddr,
-						unsigned long *poffs,
-						char **modname, char *namebuf)
+unsigned long find_instruction_boundary(unsigned long saddr,
+					unsigned long *poffs,
+					char **modname, char *namebuf)
 {
 	kprobe_opcode_t buf[MAX_INSN_SIZE];
 	unsigned long offs, addr, fixed;
