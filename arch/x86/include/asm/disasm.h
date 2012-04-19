fda1d53b884fc4bcc31b707fe95cc4b9805ab04c
Masami Hiramatsu <masami.hiramatsu@gmail.com>
2012-04-20 00:39:08 +0900

    x86: Online disassemble syntax option
    Add an option for selecting disassemble syntax.

--- arch/x86/include/asm/disasm.h
+++ arch/x86/include/asm/disasm.h
@@ -8,7 +8,10 @@ extern const char *get_prefix_name(struct insn *insn);
 
 #define DISASM_STR_LEN	128
 
+#define DISASM_SYNTAX_INTEL	0
+#define DISASM_SYNTAX_ATT	1
+
 /* Disassemble given decoded instruction */
-extern int disassemble(char *buf, size_t len, struct insn *insn);
+extern int disassemble(char *buf, size_t len, struct insn *insn, int syntax);
 
 #endif	/*__X86_DISASM_H__*/
