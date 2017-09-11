d0977a15012cb55a9fc085425245cb9af09d5966
Masami Hiramatsu <mhiramat@kernel.org>
2017-09-11 15:31:07 +0900

    arm: Cleanup in_exception_text() and move it in asm/sections.h
    Cleanup in_exception_text() using memory_contains() and
    move it in asm/sections.h from asm/trap.h because
    section symbols and memory_contains() are defined in
    asm/sections.h.
    
    Signed-off-by: Masami Hiramatsu <mhiramat@kernel.org>

--- arch/arm/include/asm/sections.h
+++ arch/arm/include/asm/sections.h
@@ -4,5 +4,22 @@
 #include <asm-generic/sections.h>
 
 extern char _exiprom[];
+extern char __exception_text_start[], __exception_text_end[];
+
+/**
+ * in_exception_text - check if an address is in exception_text or
+ *			irqentry_text
+ * @addr: virtual address to be checked
+ *
+ * Returns: true if the address specified by @addr is in the exception_text or
+ * irqentry_text, false otherwise.
+ */
+static inline bool in_exception_text(unsigned long addr)
+{
+	return memory_contains(__exception_text_start, __exception_text_end,
+			       (void *)addr, 0) ||
+		memory_contains(__irqentry_text_start, __irqentry_text_end,
+				(void *)addr, 0);
+}
 
 #endif	/* _ASM_ARM_SECTIONS_H */
