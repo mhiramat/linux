ff4b96aa3d473cc1a9fa8333b7eaba5d5822a669
Masami Hiramatsu <mhiramat@kernel.org>
2019-11-28 09:36:13 +0900

    tools: bootconfig: Add bootconfig command
    Add "bootconfig" command which operates the bootconfig
    config-data on initrd image.
    
    User can add/delete/verify the boot config on initrd
    image using this command.
    
    e.g.
    Add a boot config to initrd image
     # bootconfig -a myboot.conf /boot/initrd.img
    
    Remove it.
     # bootconfig -d /boot/initrd.img
    
    Or verify (and show) it.
     # bootconfig /boot/initrd.img
    
    Signed-off-by: Masami Hiramatsu <mhiramat@kernel.org>

---
+++ tools/bootconfig/include/linux/printk.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _SKC_LINUX_PRINTK_H
+#define _SKC_LINUX_PRINTK_H
+
+#include <stdio.h>
+
+#define printk	printf
+#define pr_err printf
+#define pr_warn	printf
+#define pr_info	printf
+#define pr_debug printf
+
+#endif
