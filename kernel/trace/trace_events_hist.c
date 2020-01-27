1ada8843e4c05f6f995b19ce6e6853e974ef2d0a
Masami Hiramatsu <mhiramat@kernel.org>
2020-01-27 11:30:56 +0900

    tracing/boot: Add synthetic event support
    [ upstream 3fbe2d6e1fce255d918b622fb2af22e98364a154 ]
    
    Add synthetic event node support to boot time tracing.
    The synthetic event is a kind of event node, but the group
    name is "synthetic".
    
     - ftrace.event.synthetic.EVENT.fields = FIELD[, FIELD2...]
       Defines new synthetic event with FIELDs. Each field should be
       "type varname".
    
    The synthetic node requires "fields" string arraies, which defines
    the fields as same as tracing/synth_events interface.
    
    Link: http://lkml.kernel.org/r/157867241236.17873.12411615143321557709.stgit@devnote2
    
    Signed-off-by: Masami Hiramatsu <mhiramat@kernel.org>
    Signed-off-by: Steven Rostedt (VMware) <rostedt@goodmis.org>

--- kernel/trace/trace_events_hist.c
+++ kernel/trace/trace_events_hist.c
@@ -1147,6 +1147,11 @@ static int create_synth_event(int argc, char **argv)
 	return ret;
 }
 
+int synth_event_run_command(const char *command)
+{
+	return trace_run_command(command, create_synth_event);
+}
+
 static int release_all_synth_events(void)
 {
 	struct synth_event *event, *e;
