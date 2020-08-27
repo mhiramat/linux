be077369771b10d8da4f9522626af400289c518c
Peter Zijlstra <peterz@infradead.org>
2020-08-27 18:12:38 +0200

    llist: Add nonatomic __llist_add()
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>

--- include/linux/llist.h
+++ include/linux/llist.h
@@ -197,6 +197,16 @@ static inline struct llist_node *llist_next(struct llist_node *node)
 extern bool llist_add_batch(struct llist_node *new_first,
 			    struct llist_node *new_last,
 			    struct llist_head *head);
+
+static inline bool __llist_add_batch(struct llist_node *new_first,
+				     struct llist_node *new_last,
+				     struct llist_head *head)
+{
+	new_last->next = head->first;
+	head->first = new_first;
+	return new_last->next == NULL;
+}
+
 /**
  * llist_add - add a new entry
  * @new:	new entry to be added
@@ -207,6 +217,11 @@ extern bool llist_add_batch(struct llist_node *new_first,
 static inline bool llist_add(struct llist_node *new, struct llist_head *head)
 {
 	return llist_add_batch(new, new, head);
+}
+
+static inline bool __llist_add(struct llist_node *new, struct llist_head *head)
+{
+	return __llist_add_batch(new, new, head);
 }
 
 /**
