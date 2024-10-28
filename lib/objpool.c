2bbb1ca29b5186e01205057456f98bfc3b98ae6b
Masami Hiramatsu (Google) <mhiramat@kernel.org>
2024-10-28 10:07:31 +0900

    objpool: Fix to make percpu slot allocation more robust
    Since gfp & GFP_ATOMIC == GFP_ATOMIC is true for GFP_KERNEL | GFP_HIGH,
    it will use kmalloc if user specifies that combination. Here the reason
    why combining the __vmalloc_node() and kmalloc_node() is that the vmalloc
    does not support all GFP flag, especially GFP_ATOMIC. So we should check
    if gfp & (GFP_ATOMIC | GFP_KERNEL) != GFP_ATOMIC for vmalloc first. This
    ensures caller can sleep.
    And for the robustness, even if vmalloc fails, it should retry with
    kmalloc to allocate it.
    
    Fixes: aff1871bfc81 ("objpool: fix choosing allocation for percpu slots")
    Signed-off-by: Masami Hiramatsu (Google) <mhiramat@kernel.org>

--- lib/objpool.c
+++ lib/objpool.c
@@ -74,13 +74,18 @@ objpool_init_percpu_slots(struct objpool_head *pool, int nr_objs,
 		 * warm caches and TLB hits. in default vmalloc is used to
 		 * reduce the pressure of kernel slab system. as we know,
 		 * mimimal size of vmalloc is one page since vmalloc would
-		 * always align the requested size to page size
+		 * always align the requested size to page size.
 		 */
-		if ((pool->gfp & GFP_ATOMIC) == GFP_ATOMIC)
-			slot = kmalloc_node(size, pool->gfp, cpu_to_node(i));
-		else
+		slot = NULL;
+		if ((pool->gfp & (GFP_ATOMIC | GFP_KERNEL)) != GFP_ATOMIC)
 			slot = __vmalloc_node(size, sizeof(void *), pool->gfp,
 				cpu_to_node(i), __builtin_return_address(0));
+		/*
+		 * but if vmalloc fails or it is not available (e.g. GFP_ATOMIC)
+		 * allocate percpu slot with kmalloc.
+		 */
+		if (!slot)
+			slot = kmalloc_node(size, pool->gfp, cpu_to_node(i));
 		if (!slot)
 			return -ENOMEM;
 		memset(slot, 0, size);
